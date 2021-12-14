9ab5d5e776788ebb9e3209d7bc6e60b451f3c0e3
MD NAZIM UDDIN <nushourav4@gmail.com>
2021-12-14 11:56:22 +0900

    member

--- src/Components/Pages/Committee/Committee/Committee.js
+++ src/Components/Pages/Committee/Committee/Committee.js
@@ -1,11 +1,43 @@
+import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/system';
 import React from "react";
-import Navbar from "../../../Shared/NavBar/Navbar";
-
+import "./Committee.css";
 const Committee = () => {
   return (
-    <div>
-      <h4>This is committee</h4>
-    </div>
+    <Container sx={{my:5}}>
+      <Box component="h2" sx={{ flexGrow: 1 }}>Committee</Box>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(8)).map((_, index) => (
+          <Grid item xs={2} sm={3} md={3} key={index}>
+            <div class="our-team">
+              <div class="pic">
+                <img src="https://i.ibb.co/8x9xK4H/team.jpg" alt=""/>
+              </div>
+              <div class="team-content">
+                <h3 class="title">Team 1</h3>
+                <span class="post">Inhaber & Geschäftsführer</span>
+              </div>
+              <ul class="social">
+                <li>
+                  <a href="#" target="_blank" class="fab fa-facebook"></a>
+                </li>
+                <li>
+                  <a href="#" class="fab fa-twitter"></a>
+                </li>
+                <li>
+                  <a href="#" class="fab fa-google-plus"></a>
+                </li>
+                <li>
+                  <a href="#" class="fab fa-linkedin"></a>
+                </li>
+              </ul>
+            </div>
+
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
--- src/Components/Pages/Member/Member/Member.js
+++ src/Components/Pages/Member/Member/Member.js
@@ -1,11 +1,39 @@
+import { Container, Grid } from "@mui/material";
 import React from "react";
-import Navbar from "../../../Shared/NavBar/Navbar";
+import "./Member.css";
 
 const Member = () => {
   return (
-    <div>
-      <h4>Member Directory</h4>
-    </div>
+    <Container sx={{my:5}}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={3} md={3} key={index}>
+             <div class="team-wrap">
+                  <div class="team-member text-center">
+                    <div class="team-img">
+                      <img src="http://deothemes.com/envato/enigma/html/img/team_2.jpg" alt=""/>
+                        <div class="overlay">
+                          <div class="team-details text-center">
+                            <p>
+                              Our web design team will spend time with our digital marketing team.
+                            </p>
+                            <div class="socials mt-20">
+                              <a href="#"><i class="fab fa-facebook"></i></a>
+                              <a href="#"><i class="fab fa-twitter"></i></a>
+                              <a href="#"><i class="fab fa-google-plus"></i></a>
+                              <a href="#"><i class="fab fa-envelope"></i></a>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <h6 class="team-title">Steve Roberts</h6>
+                    <span>Photographer</span>
+                  </div>
+                </div>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
